891d6a375c6ae24c36ef517de26c0325f84d59d1
prastoin <paul@twenty.com>
2026-04-17 08:35:45 +0200

    fix(server): set AddIsInitialToUpgradeMigration1775909335324 name for upgrade backfill

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1775909335324-add-is-initial-to-upgrade-migration.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1775909335324-add-is-initial-to-upgrade-migration.ts
@@ -3,6 +3,8 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddIsInitialToUpgradeMigration1775909335324
   implements MigrationInterface
 {
+  name = 'AddIsInitialToUpgradeMigration1775909335324';
+
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "core"."upgradeMigration" ADD "isInitial" boolean NOT NULL DEFAULT false`,
