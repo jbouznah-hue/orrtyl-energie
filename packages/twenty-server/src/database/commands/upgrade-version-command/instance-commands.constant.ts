00f75c88d8363346464390d18ac4f5d224da9435
neo773 <neo773@protonmail.com>
2026-04-20 14:12:49 +0530

    move upgrade command to 1.24 and change prop to string from children

--- packages/twenty-server/src/database/commands/upgrade-version-command/instance-commands.constant.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/instance-commands.constant.ts
@@ -13,7 +13,7 @@ import { AddCreditBalanceToBillingCustomerFastInstanceCommand } from 'src/databa
 import { BackfillWorkspaceIdOnIndirectEntitiesSlowInstanceCommand } from 'src/database/commands/upgrade-version-command/1-22/1-22-instance-command-slow-1775758621018-backfill-workspace-id-on-indirect-entities';
 import { DropWorkspaceVersionColumnFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-23/1-23-instance-command-fast-1785000000000-drop-workspace-version-column';
 import { AddGlobalObjectContextToCommandMenuItemAvailabilityTypeFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-23/1-23-instance-command-fast-1776090711153-add-global-object-context-to-command-menu-item-availability-type';
-import { SetCalendarEventDescriptionDisplayedMaxRowsFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-23/1-23-instance-command-fast-1786000000000-set-calendar-event-description-displayed-max-rows';
+import { SetCalendarEventDescriptionDisplayedMaxRowsFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-24/1-24-instance-command-fast-1786000000000-set-calendar-event-description-displayed-max-rows';
 import { AddPageLayoutIdToCommandMenuItemFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-23/1-23-instance-command-fast-1776168404836-add-page-layout-id-to-command-menu-item';
 import { AddConditionalAvailabilityExpressionToPageLayoutWidgetFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-23/1-23-instance-command-fast-1775654781000-add-conditional-availability-expression-to-page-layout-widget';
 
